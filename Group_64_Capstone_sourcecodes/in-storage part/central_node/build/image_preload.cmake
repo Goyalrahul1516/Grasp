72a61455ecfcec9261c163253d3db01afb8dfe7a
Rahul Goyal <87524423+Goyalrahul1516@users.noreply.github.com>
2024-11-10 22:31:57 +0530

    final||

---
+++ Group_64_Capstone_sourcecodes/in-storage part/central_node/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/ncs/v2.6.1/zephyr/samples/basic/central_node/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "C:/ncs/v2.6.1/zephyr/samples/basic/central_node/nrf52dk_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
